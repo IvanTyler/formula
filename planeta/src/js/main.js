2832b6390eea6fd3a468c71769e2146a59b7ae89
fedorov.iv <Ivan_567.92@mail.ru>
2024-12-09 18:00:06 +0300

    layout ctructure

--- planeta/src/js/main.js
+++ planeta/src/js/main.js
@@ -1,1 +1,10 @@
 import '../scss/style.scss'
+
+const staffStructure = document?.querySelector('.staff-structure')
+
+staffStructure?.addEventListener('click', (e) => {
+
+    if (e.target.classList === 'staff-structure-item__circle-wrapper' || 'staff-structure-item__circle') {
+        console.log(e.target.classList)
+    }
+})
